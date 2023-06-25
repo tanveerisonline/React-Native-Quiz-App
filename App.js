f8b429b0f94cf172682f27d7b0c031971b86d76f
tanveerisonline <tanveerisonline@gmail.com>
2023-06-25 12:39:23 +0530

    Add Image and Text using Props

--- App.js
+++ App.js
@@ -8,10 +8,22 @@ export default function App() {
       <StatusBar style="auto" />
       <Text style={styles.title}>Which of these is the "glass"?</Text>
       <View style={styles.optionsContainer}>
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
+        <ImageOption
+          image="https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/glass.png"
+          text="Glass"
+        />
+        <ImageOption
+          image="https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png"
+          text="Cup"
+        />
+        <ImageOption
+          image="https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/milk.png"
+          text="Cup"
+        />
+        <ImageOption
+          image="https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/coffee.png"
+          text="Cup"
+        />
       </View>
     </View>
   );
